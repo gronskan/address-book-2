ddfc684e716b004fe1530381969a17300a751045
jg <stratacast@comcast.net>
2019-07-29 19:53:34 -0700

    add forms to input email and home address and display within the contact information. fixed delete butto to delete contact when clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,7 @@ AddressBook.prototype.findContact = function(id) {
 
 AddressBook.prototype.deleteContact = function(id) {
   for (var i=0; i<this.contacts.length; i++) {
-    if (this.contacts[id]) {
+    if (this.contacts[i]) {
       if (this.contacts[i].id == id) {
       delete this.contacts[i];
       return true;
@@ -38,10 +38,12 @@ AddressBook.prototype.deleteContact = function(id) {
 }
 
 // Business Logic for Contacts --------
-function Contact(firstName, lastName, phoneNumber) {
+function Contact(firstName, lastName, phoneNumber, emailAddress, homeAddress) {
   this.firstName = firstName,
   this.lastName = lastName,
   this.phoneNumber = phoneNumber
+  this.emailAddress = emailAddress
+  this.homeAddress = homeAddress
 }
 
 Contact.prototype.fullName = function() {
@@ -66,6 +68,8 @@ function showContact(contactId) {
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
+  $(".email-address").html(contact.emailAddress)
+  $(".home-address").html(contact.homeAddress);
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
@@ -90,12 +94,16 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var inputtedEmailAddress = $("input#new-email-address").val();
+    var inputtedHomeAddress = $("input#new-home-address").val();
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
+    $("input#new-email-address").val("");
+    $("input#new-home-address").val("");
 
-    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, inputtedHomeAddress);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
   })
